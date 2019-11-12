2fe68662f77f3d28c80d9ff0ce55d9b64cb671d5
Jacob Erickson <jarus.erickson@gmail.com>
2019-11-12 17:56:06 -0500

    update annotations format

--- dataset/get_annotations_file.py
+++ dataset/get_annotations_file.py
@@ -1,27 +1,29 @@
-""" Annotation functions """
+""" Create annotation file from manual annotations """
 import json
-import shutil
-from os import listdir, remove
+from os import listdir
 from xml.dom import minidom
 from collections import defaultdict
 
 def extract_annotation_info():
     """ Extract annotation info from Pascal VOC format xml to JSON for easy use. """
     get_xml_val = lambda x, y: x.getElementsByTagName(y)[0].firstChild.data
-    annotations = defaultdict(defaultdict)
+    annotations = defaultdict(lambda: defaultdict(list))
     for char in listdir('./annotations'):
         for fname in listdir(f'./annotations/{char}'):
             info = minidom.parse(f'./annotations/{char}/{fname}')
 
-            char_obj = info.getElementsByTagName('object')[0]
-            bndbox = char_obj.getElementsByTagName('bndbox')[0]
+            char_obj = info.getElementsByTagName('object')  # multiple bndboxes
+            for char_o in char_obj:
+                bndbox = char_o.getElementsByTagName('bndbox')[0]
 
-            ann = {
-                'xmin': get_xml_val(bndbox, 'xmin'),
-                'ymin': get_xml_val(bndbox, 'ymin'),
-                'xmax': get_xml_val(bndbox, 'xmax'),
-                'ymax': get_xml_val(bndbox, 'ymax')}
-            annotations[get_xml_val(char_obj, 'name')][fname.split('.')[0]] = ann
+                ann = {
+                    'name': get_xml_val(char_o, 'name'),
+                    'xmin': get_xml_val(bndbox, 'xmin'),
+                    'ymin': get_xml_val(bndbox, 'ymin'),
+                    'xmax': get_xml_val(bndbox, 'xmax'),
+                    'ymax': get_xml_val(bndbox, 'ymax')}
+                # Under char's picture at fname, add the bounding box
+                annotations[char][fname.split('.')[0]].append(ann)
 
     with open('./annotations.json', 'w') as outfile:
         json.dump(annotations, outfile)
@@ -29,4 +31,3 @@ def extract_annotation_info():
 if __name__ == '__main__':
     # Extract info from included annotations folder.
     extract_annotation_info()
-
