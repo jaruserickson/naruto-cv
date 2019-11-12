f9b754069db4c737c51b3f2966890aefd03e43df
Jacob Erickson <jarus.erickson@gmail.com>
2019-11-12 17:24:53 -0500

    annotate google images ds

--- dataset/get-image-links.sh
+++ dataset/get-image-links.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-googleimagesdownload -kf characters.txt -l 30 -f jpg -f png -sa -nd > google_images.txt
+googleimagesdownload -kf characters.txt -l 30 -f jpg -f png -sa -nd -iu clipartwiki.com,forum-narutoen.oasgames.com > google_images.txt
