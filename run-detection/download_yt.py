c857250910d41d73f408947dcb4811a13af595a7
Jacob Erickson <jarus.erickson@gmail.com>
2019-11-21 20:59:43 -0500

    clean

--- run-detection/download_yt.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-
-# - [Naruto Shippuden Opening 18](https://www.youtube.com/watch?v=HdgD7E6JEE4)
-#  - [Naruto Opening 2](https://www.youtube.com/watch?v=SRn99oN1p_c)
-#  - [Naruto and Hinata Wedding](https://www.youtube.com/watch?v=BoMBsDIGkKI)
-
-yt = YouTube('https://www.youtube.com/watch?v=SRn99oN1p_c').streams.first().download()
